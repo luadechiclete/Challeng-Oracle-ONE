6240f3d3219a18d2ee4cb19c02a649d6ecb54cea
Luana <santosmeiraluana@gmail.com>
2022-12-27 17:48:22 -0300

    concluido

--- file.js
+++ file.js
@@ -4,7 +4,7 @@ var outInput = document.querySelector("#output");
 
 function criptografar(){
 
-  var texto = textInput.value;
+  var texto = textInput.value.toLowerCase();
 
   var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
 
@@ -15,16 +15,16 @@ function criptografar(){
 
 function descriptografar(){
 
-    var texto = textInput.value;
+    var texto = textInput.value.toLowerCase();
 
     var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-  
+    
     document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescripto + 
     '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
 function copiar() {
-    var textoCop = document.getElementById('input-texto');
+    var textoCop = document.getElementById('output');
   
     textoCop.select();
     document.execCommand('copy');
